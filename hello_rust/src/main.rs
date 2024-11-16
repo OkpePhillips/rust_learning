194edd1314b0e25867670cb0a3c1bf56880128b0
Godwin Phillips Okpe <okpegodwin18@yahoo.com>
2024-11-16 07:41:52 +0100

    Added the files properly now

---
+++ hello_rust/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+    let first_variable = "Godwin";
+
+    println!("The value of  first_variable is {}", first_variable);
+
+    // Mutabilit in rust
+    let mut x = 5;
+    println!("Initial valuel of x is {}", x);
+
+    x = 10;
+    println!("The mutated value of x is {}", x);
+    
+}
